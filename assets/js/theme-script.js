eb836d5978870daabe5bcbff3cb968c2c31a3a85
Jay B <thtan0407@gmail.com>
2021-02-10 15:16:28 +0700

    1

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -12,7 +12,179 @@ var call_noti = function (msg, type, time, position) {
 };
 
 $(document).ready(function () {
+<<<<<<< HEAD
 	let favourite = new Swiper('#swiper-newspaper', {
+=======
+	$('#banner .slider').slick({
+		autoplay: false,
+		speed: 3000,
+		autoplaySpeed: 10000,
+		lazyLoad: 'progressive',
+		arrows: true,
+		dots: false,
+		prevArrow: '<div class="slick-nav prev-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+		nextArrow: '<div class="slick-nav next-arrow"><i></i><svg><use xlink:href="#circle"></svg></div>',
+	}).slickAnimation();
+	$('#banner .slick-nav').on('click touch', function (e) {
+		e.preventDefault();
+		var arrow = $(this);
+		if (!arrow.hasClass('animate')) {
+			arrow.addClass('animate');
+			setTimeout(() => {
+				arrow.removeClass('animate');
+			}, 1600);
+		}
+	});
+	
+	var newspapers = new Swiper('#newspapers', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.intro .bottom .swiper-button-next',
+			prevEl: '.intro .bottom .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 30,
+			},
+			1024: {
+				slidesPerView: 3,
+				spaceBetween: 30,
+			},
+		}
+	});
+	
+	var activities = new Swiper('#activities', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.activities .swiper-button-next',
+			prevEl: '.activities .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+		}
+	});
+	var award = new Swiper('#award', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.award .swiper-button-next',
+			prevEl: '.award .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+		}
+	});
+	var newspapers = new Swiper('#newspapers', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.newspapers .swiper-button-next',
+			prevEl: '.newspapers .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+		}
+	});
+	var story = new Swiper('#story', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.story .swiper-button-next',
+			prevEl: '.story .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+		}
+	});
+	
+	var best = new Swiper('#product-best', {
+		slidesPerView: 3,
+		navigation: {
+			nextEl: '.single-product--best .swiper-button-next',
+			prevEl: '.single-product--best .swiper-button-prev',
+		},
+		autoplay: {
+			delay: 10000,
+			disableOnInteraction: false,
+		},
+		breakpoints: {
+			320: {
+				slidesPerView: 1,
+			},
+			768: {
+				slidesPerView: 2,
+				spaceBetween: 15,
+			},
+			1024: {
+				slidesPerView: 5,
+				spaceBetween: 10,
+			},
+		}
+	});
+	
+	var favourite = new Swiper('#swiper-favourite', {
+>>>>>>> parent of 9350943 (Xong)
 		slidesPerView: 1,
 		pagination: {
 			el: '.swiper-pagination',
