f2b26bdc2144085c47343306862fe44c8c055147
Jay B <thtan0407@gmail.com>
2021-02-10 15:25:44 +0700

    Test

--- assets/js/theme-script.js
+++ assets/js/theme-script.js
@@ -12,9 +12,36 @@ var call_noti = function (msg, type, time, position) {
 };
 
 $(document).ready(function () {
-<<<<<<< HEAD
-	let favourite = new Swiper('#swiper-newspaper', {
-=======
+	let windowWidth = $(window).width();
+	if (windowWidth < 992) {
+		$("header .header-bottom .left-side > ul > li > ul").each(function (index) {
+			$(this).prev().attr({
+				"href": "#subMenu" + index,
+				"data-toggle": "collapse"
+			});
+			$(this).attr({
+				"id": "subMenu" + index,
+				"class": "collapse",
+				"data-parent": "#hasMenu"
+			});
+		})
+	}
+	
+	$('header .header-bottom .left-side > ul').prepend('<li><a href="javascript:void(0)" class="d-flex d-lg-none" id="close-menu"><i class="fal fa-times"></i></a></li>');
+	
+	function callMenu() {
+		if ($('body').hasClass('show-navigation')) {
+			$('body').removeClass('show-navigation');
+		} else {
+			$('body').addClass('show-navigation');
+		}
+	}
+	
+	$(document).on("click", "#hamburger, #close-menu, .header-overlay", function () {
+		callMenu();
+	});
+	
+	
 	$('#banner .slider').slick({
 		autoplay: false,
 		speed: 3000,
@@ -170,11 +197,12 @@ $(document).ready(function () {
 		},
 		breakpoints: {
 			320: {
-				slidesPerView: 1,
+				slidesPerView: 2,
+				spaceBetween: 10,
 			},
 			768: {
-				slidesPerView: 2,
-				spaceBetween: 15,
+				slidesPerView: 3,
+				spaceBetween: 10,
 			},
 			1024: {
 				slidesPerView: 5,
@@ -184,15 +212,58 @@ $(document).ready(function () {
 	});
 	
 	var favourite = new Swiper('#swiper-favourite', {
->>>>>>> parent of 9350943 (Xong)
 		slidesPerView: 1,
-		pagination: {
-			el: '.swiper-pagination',
-			clickable: true,
+		navigation: {
+			nextEl: '.sidebar-favourite .swiper-button-next',
+			prevEl: '.sidebar-favourite .swiper-button-prev',
 		},
 		autoplay: {
 			delay: 10000,
 			disableOnInteraction: false,
 		},
 	});
+	
+	//	Thêm giỏ hàng
+	$('.addCart').click(function (e) {
+		e.preventDefault();
+		let amount = $('#numberCart');
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+			amount.text(parseInt(amount.text()) - 1);
+		} else {
+			$(this).addClass('active');
+			amount.text(parseInt(amount.text()) + 1);
+		}
+		
+		call_noti("Cập nhật giỏ hàng thành công", 'success', 2000);
+	});
+	
+	//	Yêu thích
+	$('.addFavourite').click(function (e) {
+		e.preventDefault();
+		if ($(this).hasClass('active')) {
+			$(this).removeClass('active');
+		} else {
+			$(this).addClass('active');
+		}
+		
+		call_noti("Cập nhật yêu thích thành công", 'success', 2000);
+	});
+	
+	//  Hiệu ứng input form
+	$('.input-effect').blur(function () {
+		$(this).val() != '' ? $(this).addClass('valid') : $(this).removeClass('valid');
+	});
+	if ($('.input-effect option:selected')) {
+		$('.input-effect option:selected').parent().addClass('valid')
+	}
+	
+	$('.modal-theme').on('hide.bs.modal', function () {
+		$('#tabResgister').removeClass('show active');
+		$('#tabLogin').addClass('show active');
+	});
+	
+	$('.modal-theme [data-toggle=tab]').click(function (e) {
+		$('.modal-theme [data-toggle=tab]').removeClass('active');
+	})
 })
